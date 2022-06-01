32ffdbc01778edb865a39c3c216ed743694d2dd1
pejman <pejman.hkh@gmail.com>
2022-06-01 10:30:34 +0430

    e

--- app/Model/Movies.php
+++ app/Model/Movies.php
@@ -44,11 +44,48 @@ class Movies extends \Peji\DB\Model {
 		return Ratings::sql("where tconst = ? ")->findFirst([ $this->code ]);
 	}
 
+	function request( $url ) {
+	
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url );
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+		curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+		$headers = array();
+		$headers[] = 'Authority: www.imdb.com';
+		$headers[] = 'Pragma: no-cache';
+		$headers[] = 'Cache-Control: no-cache';
+		$headers[] = 'Sec-Ch-Ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"';
+		$headers[] = 'Sec-Ch-Ua-Mobile: ?0';
+		$headers[] = 'Sec-Ch-Ua-Platform: \"Linux\"';
+		$headers[] = 'Upgrade-Insecure-Requests: 1';
+		$headers[] = 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36';
+		$headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+		$headers[] = 'Sec-Fetch-Site: same-origin';
+		$headers[] = 'Sec-Fetch-Mode: navigate';
+		$headers[] = 'Sec-Fetch-User: ?1';
+		$headers[] = 'Sec-Fetch-Dest: document';
+		$headers[] = 'Accept-Language: en-US,en;q=0.9,fa;q=0.8';
+	
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		$result = curl_exec($ch);
+		if (curl_errno($ch)) {
+		    echo 'Error:' . curl_error($ch);
+		}
+		curl_close($ch);
+		
+		return $result;
+	}
+
 	function update() {
 
 		$movie = $this;
 
-		$ret = file_get_contents( $this->imdbUrl );
+		$ret = $this->request( $this->imdbUrl );
 		$html = str_get_html( $ret );
 
 	
