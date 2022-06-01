43363637d8b2eb63c73a5290e3d6fff95fb39455
pejman <pejman.hkh@gmail.com>
2022-06-01 11:07:53 +0430

    e

--- app/Controller/User/indexController.php
+++ app/Controller/User/indexController.php
@@ -24,7 +24,7 @@ class indexController extends appController {
 		array_shift( $params );
 		$params = $this->keyPairParams( $params );
 
-		$movies = Basics::sql(" select id,tconst from basics as a right join ratings as b on a.tconst = b.tconst where 1 order by ( b.averageRating * b.numVotes ) desc ")->paginate( (int)(isset($this->get['npage'])?$this->get['npage']:36), @$params['page']?:1 )->find();
+		$movies = Basics::sql(" select a.id,a.tconst from basics as a right join ratings as b on a.tconst = b.tconst where 1 order by ( b.averageRating * b.numVotes ) desc ")->paginate( (int)(isset($this->get['npage'])?$this->get['npage']:36), @$params['page']?:1 )->find();
 
 		$path = explode("/", getPath() );
 		$this->set('mcontroller', $path[0]?:'index' );
